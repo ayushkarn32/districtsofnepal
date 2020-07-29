8f4a51028a811a8e73c86fce412c62dac9121884
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2020-07-30 01:59:54 +0545

    Create district.sql

---
+++ district.sql
@@ -0,0 +1,539 @@
+/* CREATE TABLE */
+CREATE TABLE districts(
+id INT,
+title VARCHAR(100),
+province INT
+);
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+1, 'Achham', 7
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+2, 'Arghakhanchi', 5
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+3, 'Baglung', 4
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+4, 'Baitadi', 7
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+5, 'Bajhang', 7
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+6, 'Bajura', 7
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+7, 'Banke', 5
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+8, 'Bara', 2
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+9, 'Bardiya', 5
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+10, 'Bhaktapur', 3
+);
+
+/* INSERT QUERY NO: 11 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+11, 'Bhojpur', 1
+);
+
+/* INSERT QUERY NO: 12 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+12, 'Chitwan', 3
+);
+
+/* INSERT QUERY NO: 13 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+13, 'Dadeldhura', 7
+);
+
+/* INSERT QUERY NO: 14 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+14, 'Dailekh', 6
+);
+
+/* INSERT QUERY NO: 15 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+15, 'Dang', 5
+);
+
+/* INSERT QUERY NO: 16 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+16, 'Darchula', 7
+);
+
+/* INSERT QUERY NO: 17 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+17, 'Dhading', 3
+);
+
+/* INSERT QUERY NO: 18 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+18, 'Dhankuta', 1
+);
+
+/* INSERT QUERY NO: 19 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+19, 'Dhanusa', 2
+);
+
+/* INSERT QUERY NO: 20 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+20, 'Dolakha', 3
+);
+
+/* INSERT QUERY NO: 21 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+21, 'Dolpa', 6
+);
+
+/* INSERT QUERY NO: 22 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+22, 'Doti', 7
+);
+
+/* INSERT QUERY NO: 23 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+23, 'Gorkha', 4
+);
+
+/* INSERT QUERY NO: 24 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+24, 'Gulmi', 5
+);
+
+/* INSERT QUERY NO: 25 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+25, 'Humla', 6
+);
+
+/* INSERT QUERY NO: 26 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+26, 'Ilam', 1
+);
+
+/* INSERT QUERY NO: 27 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+27, 'Jajarkot', 6
+);
+
+/* INSERT QUERY NO: 28 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+28, 'Jhapa', 1
+);
+
+/* INSERT QUERY NO: 29 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+29, 'Jumla', 6
+);
+
+/* INSERT QUERY NO: 30 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+30, 'Kailali', 7
+);
+
+/* INSERT QUERY NO: 31 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+31, 'Kalikot', 6
+);
+
+/* INSERT QUERY NO: 32 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+32, 'Kanchanpur', 7
+);
+
+/* INSERT QUERY NO: 33 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+33, 'Kapilbastu', 5
+);
+
+/* INSERT QUERY NO: 34 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+34, 'Kaski', 4
+);
+
+/* INSERT QUERY NO: 35 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+35, 'Kathmandu', 3
+);
+
+/* INSERT QUERY NO: 36 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+37, 'Kavrepalanchok', 3
+);
+
+/* INSERT QUERY NO: 37 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+38, 'Khotang', 1
+);
+
+/* INSERT QUERY NO: 38 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+39, 'Lalitpur', 3
+);
+
+/* INSERT QUERY NO: 39 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+40, 'Lamjung', 4
+);
+
+/* INSERT QUERY NO: 40 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+41, 'Mahottari', 2
+);
+
+/* INSERT QUERY NO: 41 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+42, 'Makwanpur', 3
+);
+
+/* INSERT QUERY NO: 42 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+43, 'Manang', 4
+);
+
+/* INSERT QUERY NO: 43 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+44, 'Morang', 1
+);
+
+/* INSERT QUERY NO: 44 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+45, 'Mugu', 6
+);
+
+/* INSERT QUERY NO: 45 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+46, 'Mustang', 4
+);
+
+/* INSERT QUERY NO: 46 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+47, 'Myagdi', 4
+);
+
+/* INSERT QUERY NO: 47 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+48, 'Nawalparasi West', 5
+);
+
+/* INSERT QUERY NO: 48 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+49, 'Nuwakot', 3
+);
+
+/* INSERT QUERY NO: 49 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+50, 'Okhaldhunga', 1
+);
+
+/* INSERT QUERY NO: 50 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+51, 'Palpa', 5
+);
+
+/* INSERT QUERY NO: 51 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+52, 'Panchthar', 1
+);
+
+/* INSERT QUERY NO: 52 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+53, 'Parbat', 4
+);
+
+/* INSERT QUERY NO: 53 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+54, 'Parsa', 2
+);
+
+/* INSERT QUERY NO: 54 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+55, 'Pyuthan', 5
+);
+
+/* INSERT QUERY NO: 55 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+56, 'Ramechhap', 3
+);
+
+/* INSERT QUERY NO: 56 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+57, 'Rasuwa', 3
+);
+
+/* INSERT QUERY NO: 57 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+58, 'Rautahat', 2
+);
+
+/* INSERT QUERY NO: 58 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+59, 'Rolpa', 5
+);
+
+/* INSERT QUERY NO: 59 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+60, 'Rukum East', 5
+);
+
+/* INSERT QUERY NO: 60 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+61, 'Rukum West', 6
+);
+
+/* INSERT QUERY NO: 61 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+62, 'Rupandehi', 5
+);
+
+/* INSERT QUERY NO: 62 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+63, 'Salyan', 6
+);
+
+/* INSERT QUERY NO: 63 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+64, 'Sankhuwasabha', 1
+);
+
+/* INSERT QUERY NO: 64 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+65, 'Saptari', 2
+);
+
+/* INSERT QUERY NO: 65 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+66, 'Sarlahi', 2
+);
+
+/* INSERT QUERY NO: 66 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+67, 'Sindhuli', 3
+);
+
+/* INSERT QUERY NO: 67 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+68, 'Sindhupalchok', 3
+);
+
+/* INSERT QUERY NO: 68 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+69, 'Siraha', 2
+);
+
+/* INSERT QUERY NO: 69 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+70, 'Solukhumbu', 1
+);
+
+/* INSERT QUERY NO: 70 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+71, 'Sunsari', 1
+);
+
+/* INSERT QUERY NO: 71 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+72, 'Surkhet', 6
+);
+
+/* INSERT QUERY NO: 72 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+73, 'Syangja', 4
+);
+
+/* INSERT QUERY NO: 73 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+74, 'Tanahu', 4
+);
+
+/* INSERT QUERY NO: 74 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+75, 'Taplejung', 1
+);
+
+/* INSERT QUERY NO: 75 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+76, 'Terhathum', 1
+);
+
+/* INSERT QUERY NO: 76 */
+INSERT INTO districts(id, title, province)
+VALUES
+(
+77, 'Udayapur', 1
+);
+
